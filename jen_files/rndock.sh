6f9cf00f9f189ddf8df0f3d46586e8b6072e641e
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-03 13:06:35 +0300

    Update rndock.sh

--- jen_files/rndock.sh
+++ jen_files/rndock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker login -u $USERNAME -p $PASSWORD
+docker login -u $USERNAME --password-stdin $PASSWORD
 docker pull devslavus/my-first-blog
 docker run --name mfb -d -p 80:80 my-first-blog:latest
