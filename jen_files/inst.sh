b45f368449e1e9622fb0cae29fd41a7afbc1f28b
devSLAVUS <91405914+devSLAVUS@users.noreply.github.com>
2021-11-03 11:52:46 +0300

    Update inst.sh

--- jen_files/inst.sh
+++ jen_files/inst.sh
@@ -18,7 +18,7 @@ else
    echo $name" not installed"
    apt install -y apt-transport-https
    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_rele$
+   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
    apt update
    apt install docker-ce
 fi
